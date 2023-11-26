6bebba2ccaea2e5301b557de4f44ff3c5a84efd1
ameya-6964 <belvalkar7@gmail.com>
2023-11-26 22:06:20 +0530

    Rendered Movies On Homepage Using TMDB API

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -9,9 +9,34 @@ const Movie = ({ movie, i }) => {
   console.log(movie, i);
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={3} className={classes.movie}>
-      <Typography className={classes.title} variant="h5">
-        {movie.title}
-      </Typography>
+      <Grow in key={i} timeout={(i + 1) * 350}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          {movie.poster_path ? (
+            <img
+              alt={movie.title}
+              className={classes.image}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            />
+          ) : (
+            <img
+              alt={movie.title}
+              className={classes.image}
+              src="https://via.placeholder.com/400"
+            />
+          )}
+          <Typography className={classes.title} variant="h5">
+            {movie.title}
+          </Typography>
+          <Tooltip
+            disableTouchListener
+            title={`${Math.round(movie.vote_average)} /10`}
+          >
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
