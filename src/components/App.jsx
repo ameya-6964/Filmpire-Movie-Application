5c604abf327daf15920a0ee4af5b1f058be2106c
ameya-6964 <belvalkar7@gmail.com>
2023-11-29 01:53:28 +0530

    Added Alan AI Voice Assistant With Basic Commands

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import { CssBaseline } from "@mui/material";
 import { Route, Switch } from "react-router-dom";
 import useStyles from "./styles";
 
 //* ALl Component Imports
 import { Actors, Navbar, Movies, Profile, MovieInformation } from "./index";
+import useAlan from "./Alan";
 
 const App = () => {
   const classes = useStyles();
+
+  const alanBtnContainer = useRef();
+  useAlan();
   return (
     <div className={classes.root}>
       <CssBaseline />
@@ -29,6 +33,7 @@ const App = () => {
           </Route>
         </Switch>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 };
